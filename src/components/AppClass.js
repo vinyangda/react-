c42b4f112ddeb6dbc23cf108871651f6cbd73e54
vin <didekqls93@gmail.com>
2023-02-14 13:50:14 +0900

    Class components

---
+++ src/components/AppClass.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import BoxClass from "./BoxClass";
+
+export default class AppClass extends Component {
+  //constructor == lifecycle function 중 하나 class생성에 쓰임
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter2: 0,
+      num: 1,
+      value: 0,
+    };
+
+    increase = () => {
+      this.setState({
+        counter2: this.state.counter2 + 1,
+        value: this.state.value + 1,
+      });
+    };
+  }
+  render() {
+    return (
+      <div>
+        <div>{this.state.counter2}</div>
+        <button onClick={this.increase}>Click</button>
+        <BoxClass num={this.state.value} />
+      </div>
+    );
+  }
+}
